df743bbfca654cae67479898bacd78f90be13cbb
john.huang <john.huang@onelab.tw>
2024-03-20 11:54:13 +0800

    test page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,78 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { useCallback, useMemo, useRef } from 'react';
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function TabOneScreen() {
+  // hooks
+  const sheetRef = useRef<BottomSheet>(null);
+
+  // variables
+  const data = useMemo(
+    () =>
+      Array(50)
+        .fill(0)
+        .map((_, index) => `index-${index}`),
+    []
+  );
+  const snapPoints = useMemo(() => ["25%", "50%", "90%"], []);
+
+  // callbacks
+  const handleSheetChange = useCallback((index : number) => {
+    console.log("handleSheetChange", index);
+  }, []);
+
+  const handleSnapPress = useCallback((index : number) => {
+    sheetRef.current?.snapToIndex(index);
+  }, []);
+
+  const handleClosePress = useCallback(() => {
+    sheetRef.current?.close();
+  }, []);
+
+  // render
+  const renderItem = useCallback(
+    ({ item } : {item: any}) => (
+      <View style={styles.itemContainer}>
+        <Text>{item}</Text>
+      </View>
+    ),
+    []
+  );
+
   return (
-    <Text>
-      asdf
-    </Text>
+    <View style={styles.container}>
+      <Button title="Snap to 90%" onPress={() => handleSnapPress(2)} />
+      <Button title="Snap to 50%" onPress={() => handleSnapPress(1)} />
+      <Button title="Snap to 25%" onPress={() => handleSnapPress(0)} />
+      <Button title="Close" onPress={() => handleClosePress()} />
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        onChange={handleSheetChange}
+      >
+        <BottomSheetFlatList
+          data={data}
+          keyExtractor={(i) => i}
+          renderItem={renderItem}
+          contentContainerStyle={styles.contentContainer}
+        />
+      </BottomSheet>
+    </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    paddingTop: 200,
+  },
+  contentContainer: {
+    backgroundColor: "white",
+  },
+  itemContainer: {
+    padding: 6,
+    margin: 6,
+    backgroundColor: "#eee",
+  },
 });
